5fb89d2db147ac3db356554450792c1bd478f002
Michael Zupecki <mzupecki@profectusgroup.com>
2021-11-08 11:23:16 +1100

    Initial commit

---
+++ src/app.ts
@@ -0,0 +1,36 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("Name?", function(name) {
+    rl.question("Age?", function(age) {
+        console.log(`${name}, you are ${age} years old`);
+        rl.close();
+    });
+});
+
+rl.on("close", function() {
+    console.log("\nExit on rl.close()");
+    process.exit(0);
+});
+
+/*
+// express
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello Robot!');
+});
+
+app.listen(port, (err) => {
+  if(err) {
+    return console.error(err);
+  }
+  return console.log(`server listening on ${port}`);
+});
+*/
