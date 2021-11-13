8290536f7817580f520e589a725234b1da69e8fa
Michael Zupecki <michaelzupecki@gmail.com>
2021-11-13 19:28:24 +1100

    Stub out tests for logic

---
+++ src/logic.test.js
@@ -0,0 +1,32 @@
+describe("Place", () => {
+    test("it should accept 3 args", () => {
+        // test
+    });
+});
+
+describe("Move", () => {
+    test("it should increase the position by 1", () => {
+        // test
+    });
+});
+
+describe("Rotate", () => {
+    test("it should rotate left", () => {
+        // test
+    });
+    test("it should rotate right", () => {
+        // test
+    });
+});
+
+describe("Report", () => {
+    test("it should output the robots position and direction", () => {
+        // test
+    });
+});
+
+describe("Exit", () => {
+    test("it should exit", () => {
+        // test
+    });
+});
